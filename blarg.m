42b69b143cad4cdde96472a9a88f76631472fa9e
Andrew Janke <floss@apjanke.net>
2019-03-30 05:56:53 -0400

    Add minimal reproduction

---
+++ blarg.m
@@ -0,0 +1,7 @@
+function blarg
+  if 0 sldkfjdskljfskldklsdjf (x>y)
+    fprintf('yup\n');
+  else
+    fprintf('nope\n');
+  endif
+endfunction
--- ode2.m
+++ ode2.m
@@ -1,14 +1,13 @@
 function vdot = ode2(v,r)
-  keyboard
   lambda=1;
   vdot=zeros(2,1);
 #  if ((v(2))<0.00000000000000000000001) someboguskeyword (v(1)<=0)
   if 0 skldjfklsdjfklskljdsklslfsdklj (v(1)<=0)
-    fprintf('yup');
+    fprintf('yup\n');
     vdot(1)=0;
     vdot(2)=0;
   else
-    fprintf('nope');
+    fprintf('\nnope\n');
     vdot(1)=v(2);
     vdot(2)=-3*v(2)/r-lambda*7.5*v(1)^4/v(2);
   endif
